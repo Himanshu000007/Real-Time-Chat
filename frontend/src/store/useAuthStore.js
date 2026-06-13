35df5abc2174274e377db94650f1c9449a6772c9
Himanshu <himanshu8809564335@gmail.com>
2026-06-13 23:12:57 +0530

    entered backend url into frontend areas

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -147,7 +147,7 @@ export const useAuthStore = create((set, get) => ({
     const { authUser } = get();
     if (!authUser || get().socket?.connected) return;
 
-    const socket = io(BASE_URL, {
+    const socket = io("https://real-time-chat-sbjq.onrender.com", {
       withCredentials: true, // this ensures cookies are sent with the connection
     });
 
